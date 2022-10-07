dbf999817e166d5bbfc9109945b1c4026f4498be
Nikita Nikolaev <nikita.n.nikolaev@gmail.com>
2022-10-07 16:10:53 +0100

    Initial commit

---
+++ src/main/java/com/alvaria/loremipsum/LoremIpsumApplication.java
@@ -0,0 +1,28 @@
+package com.alvaria.loremipsum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class LoremIpsumApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoremIpsumApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(LoremIpsumApplication.class);
+    }
+}
---
+++ src/test/java/com/alvaria/loremipsum/LoremIpsumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alvaria.loremipsum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoremIpsumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
